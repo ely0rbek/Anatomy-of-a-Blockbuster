c3a6f516470839cbdafab517361b9dc9f256d5f5
elyorbek <temirovelyor7347@gmail.com>
2025-11-13 21:42:04 +0100

    Revenue was calculated over the years, a function was created, and the presentation was launched.

--- blockbuster_analysis/core.py
+++ blockbuster_analysis/core.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("D:\python\BlockbusterDB\\movies_metadata.csv",low_memory=False)
 
-revenues= list(map(float,df["revenue"]))
+df['revenue']= list(map(float,df["revenue"]))
 
-released_dates=pd.to_datetime(df['release_date'], errors='coerce')
+df['release_date']=pd.to_datetime(df['release_date'], errors='coerce')
 
-print(released_dates[:10])
 
-print(revenues[:10])
+
+def show_plot_revenue_by_year():
+    df['year']=df['release_date'].dt.year
+
+    static_data=df.groupby('year')['revenue'].mean().dropna()
+
+    plt.bar(static_data.index, static_data.values)
+    plt.title("Average Revenue by Year")
+    plt.xlabel("Year")
+    plt.ylabel("Average Revenue")
+
+    plt.show()
+
+
 
