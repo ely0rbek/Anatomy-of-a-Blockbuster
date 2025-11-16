353e47d4e5dbe5b8f2a494165c0897ec564b2a69
elyorbek <temirovelyor7347@gmail.com>
2025-11-16 23:06:42 +0100

    data was read from csv file and ploting func was created

--- blockbuster_analysis/genre_avg_rating.py
+++ blockbuster_analysis/genre_avg_rating.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ast, os
 
+csv_path="D:\python\BlockbusterDB\\movies_metadata.csv"
 def parse_genres(x):
     if pd.isna(x): return []
     try:
@@ -13,3 +14,8 @@ def parse_genres(x):
         return []
     return []
 
+
+def plot_genre_avg_rating(csv_path):
+    df = pd.read_csv(csv_path, low_memory=False)
+    df['genres_parsed'] = df['genres'].apply(parse_genres)
+    df['vote_average'] = pd.to_numeric(df['vote_average'], errors='coerce')
