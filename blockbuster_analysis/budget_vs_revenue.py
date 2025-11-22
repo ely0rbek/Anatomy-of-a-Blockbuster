4b4214568e95077a79761e28a7082d98b1619e5e
elyorbek <temirovelyor7347@gmail.com>
2025-11-22 15:28:49 +0100

    A graph of the relationship between budget and revenue was drawn.

--- blockbuster_analysis/budget_vs_revenue.py
+++ blockbuster_analysis/budget_vs_revenue.py
@@ -10,3 +10,42 @@ def check_float(x):
         return np.nan
     
 
+def plot_budget_vs_revenue():
+    csv_path="D:\python\Anatomy_of_Blockbuster\datasets\\movies_metadata.csv"
+    df=pd.read_csv(csv_path,low_memory=False)
+    df["budget"]=df['budget'].apply(check_float)
+    df['revenue']=df['revenue'].apply(check_float)
+    df=df[(df['budget']>0) & (df['revenue']>0)]
+
+    x=np.log10(df['budget'])
+    y=np.log10(df['revenue'])
+    coef=np.polyfit(x,y,1)
+
+    plt.figure(figsize=(8,6))
+    plt.scatter(x, y, alpha=0.3, s=10)
+    plt.plot(x, coef[0]*x + coef[1], color='red')
+    plt.xlabel("log10(Budget)")
+    plt.ylabel("log10(Revenue)")
+    plt.title(f"Budget vs Revenue (log-log)\nSlope={coef[0]:.2f}")
+    plt.tight_layout()
+    # plt.savefig("plots/budget_vs_revenue.png")
+    plt.show()
+
+
+
+
+# ------------------- this is chart without log ------------------
+    # x=df['budget']
+    # y=df['revenue']
+    # coef=np.polyfit(x,y,1)
+
+    # plt.figure(figsize=(8,6))
+    # plt.scatter(x, y, alpha=0.3, s=10)
+    # plt.plot(x, coef[0]*x + coef[1], color='red')
+    # plt.xlabel("log10(Budget)")
+    # plt.ylabel("log10(Revenue)")
+    # plt.title(f"Budget vs Revenue (log-log)\nSlope={coef[0]:.2f}")
+    # plt.tight_layout()
+    # plt.show()
+
+
