58cb01e40272ef7558c508a0cf4e96ba6116265f
elyorbek <temirovelyor7347@gmail.com>
2025-11-23 22:27:00 +0100

    A statistical table of revenues by runtime has been created.

---
+++ blockbuster_analysis/runtime_vs_revenue.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from models import Parsing, csv_paths
+
+def plot_runtime_vs_revnue():
+    csv_path=csv_paths()
+    parsing=Parsing()
+
+    df=pd.read_csv(csv_path.get_movies_path(),low_memory=False)
+
+    df['runtime']=pd.to_numeric(df['runtime'],errors='coerce')
+    df['revenue']=df['revenue'].apply(parsing.check_float)
+
+
+    # x=np.log10(df['runtime'])
+    # y=np.log10(df['revenue'])
+
+    plt.figure(figsize=(8,6))
+    plt.scatter(df['runtime'], df['revenue'], alpha=0.3, s=10)
+    # plt.scatter(x, df['revenue'], alpha=0.3, s=10)
+    plt.yscale('symlog')
+    plt.title("Runtime vs Revenue")
+    plt.xlabel("Runtime (minutes)")
+    plt.ylabel("Revenue (USD, symlog scale)")
+    plt.tight_layout()
+    # plt.savefig("plots/runtime_vs_revenue.png")
+    
+    plt.show()
+
+
+
