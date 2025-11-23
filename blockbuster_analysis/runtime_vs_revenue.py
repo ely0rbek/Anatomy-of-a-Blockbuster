78e4545e8010404b5cc69fc0fca37392fc7f815a
elyorbek <temirovelyor7347@gmail.com>
2025-11-23 22:29:08 +0100

    chart was build in presentation layer

--- blockbuster_analysis/runtime_vs_revenue.py
+++ blockbuster_analysis/runtime_vs_revenue.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-from models import Parsing, csv_paths
+from blockbuster_analysis.models import Parsing, csv_paths
 
 def plot_runtime_vs_revnue():
     csv_path=csv_paths()
